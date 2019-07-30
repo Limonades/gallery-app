f318790e36c3cb54fced64b78d23da75fce89942
Vladislav Krivokon <vladeslimonade@gmail.com>
2019-07-30 23:31:29 +0300

    Updates

---
+++ precache-manifest.cdffa40cfe6e974035d1ff42275ddcc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5463fcd29c59a76c7ed76289f83bbf5",
+    "url": "/gallery-app/index.html"
+  },
+  {
+    "revision": "1de8ffe439508874ab1f",
+    "url": "/gallery-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/gallery-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1de8ffe439508874ab1f",
+    "url": "/gallery-app/static/js/main.499b9855.chunk.js"
+  },
+  {
+    "revision": "26c986134660029d3cc4",
+    "url": "/gallery-app/static/js/runtime~main.4991d15d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gallery-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
